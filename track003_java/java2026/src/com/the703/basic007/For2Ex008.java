e8fa401cecec36b8098a11f9e3a1693470acc50d
look-93 <qhfk0961@gmail.com>
2026-04-09 22:00:00 +0900

    복습

--- track003_java/java2026/src/com/the703/basic007/For2Ex008.java
+++ track003_java/java2026/src/com/the703/basic007/For2Ex008.java
@@ -2,18 +2,46 @@ package com.the703.basic007;
 
 public class For2Ex008 {
 
-	public static void main(String[] args) {
-		int count = 1;
-		
+	public static void main(String[] args) {		
+		//for
+		System.out.println("<for>");
 		for(int i=1;i<=5;i++) {
-			for(int i1=5;i1>=i;i1--) {
-				if(count+i1==6) {
-					System.out.println(count+"+"+i1+"="+(i1+count));
-					count++;
+			for(int i1=5;i1>=1;i1--) {
+				if(i+i1==6) {
+					System.out.print(i+"+"+i1+"="+(i+i1));
 				}
 			}
 			System.out.println();
 		}
+		
+		//while
+		System.out.println("<while>");
+		int i2=1;		
+		while(i2<=5) {	
+			int i3=5;
+			while(i3>=1) {
+				if(i2+i3 ==6) {
+					System.out.print(i2+"+"+i3+"="+(i2+i3));
+				}
+				i3--;			}
+			i2++;
+			System.out.println();
+		}
+		
+		//do while
+		System.out.println("<do while>");
+		int i4=1;
+		do {
+			int i5=5;
+			do {
+				if(i4+i5==6) {
+					System.out.print(i4+"+"+i5+"="+(i4+i5));
+				}
+				i5--;
+			}while(i5>=1);
+			i4++;
+			System.out.println();
+		}while(i4<=5);
 		
 
 	}
