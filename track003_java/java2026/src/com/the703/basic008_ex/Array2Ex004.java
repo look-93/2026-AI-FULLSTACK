3bbe46bb4c91f089478bb17518a04c27d66a8124
look-93 <qhfk0961@gmail.com>
2026-04-15 23:59:46 +0900

    복습완료

--- track003_java/java2026/src/com/the703/basic008_ex/Array2Ex004.java
+++ track003_java/java2026/src/com/the703/basic008_ex/Array2Ex004.java
@@ -4,67 +4,79 @@ import java.util.Arrays;
 
 public class Array2Ex004 {
 
-	public static void main(String[] args) {
-//		int[][] result = new int[4][5];    
+	public static void main(String[] args) {    
 		int[][] datas = {  
 				{  10, 10, 10 ,10},             
 				{  20, 20, 20 ,20},     
 				{  30, 30, 30 ,30},   
 				};  // 3층 4칸
-		int widthSum=0;
-		int heightSum=0;
+
 		String st="";
 		
-//		result[0][0] = datas[0][0];
-//		result[0][1] = datas[0][1];
-//		result[0][2] = datas[0][2];
-//		result[0][3] = datas[0][3];
+		int[][] result = new int[datas.length+1][datas[0].length+1];   
+		
+//		for(int ch=0;ch<result.length;ch++) {
+//						int sum=0;
+//			for(int kan=0;kan<result[ch].length;kan++) {
+//				if(ch <=2) {
+//					if (kan <= 3 ) {
+//						int value = datas[ch][kan];
+//						result[ch][kan] = value;
+//						sum = sum+value;
+////						System.out.println(ch+","+kan+","+value);
+//					}
+//					if(kan == 4) {
+//						result[ch][kan] = sum;
+//					}	
+//				}else {
+////					00,10,20
+////					01,11,21
+////					02,12,22
+////					03,13,23
+//					result[ch][kan] = result[0][kan] + result[1][kan] + result[2][kan];
+//				}
+//				
+//			}	
+//			
+////			System.out.println(Arrays.toString(result[ch]));
+//		
+//		}
+//		
+		
+//		10,10,10,10,40
+//		20,20,20,20,80
+//		30,30,30,30,120
+//		60,60,60,60,240
+		
+//		0,0,0,0,0
+//		0,0,0,0,0
+//		0,0,0,0,0
+//		0,0,0,0,0
 
-//		
-//		result[1][0] = datas[1][0];
-//		result[1][1] = datas[1][1];
-//		result[1][2] = datas[1][2];
-//		result[1][3] = datas[1][3];
-//		result[1][4] = 80;
-//		
-//		result[2][0] = datas[2][0];
-//		result[2][1] = datas[2][1];
-//		result[2][2] = datas[2][2];
-//		result[2][3] = datas[2][3];
-//		result[2][4] = 90;
-//		
-//		result[3][0] = 60;
-//		result[3][1] = 60;
-//		result[3][2] = 60;
-//		result[3][3] = 60;
-		
-//		result[0][4] = 40;
-//		result[1][4] = 80;	
-//		result[2][4] = 90;
-//		result[3][4] = 240;		
-//		
-//		System.out.println(Arrays.toString(result[0]));
-//		System.out.println(Arrays.toString(result[1]));
-//		System.out.println(Arrays.toString(result[2]));
-//		System.out.println(Arrays.toString(result[3]));
-		
 		for(int ch=0;ch<datas.length;ch++) {
-			int[][] result = new int[datas.length+1][datas[ch].length+1];
-			
+//			int[][] result = new int[datas.length+1][datas[0].length+1];
+//			
+			int sum = 0;
 			for(int kan=0;kan<datas[ch].length;kan++) {
-				result[ch][kan] = datas[ch][kan];
-				widthSum  += datas[ch][kan];
-//				heightSum += datas[kan][kan];
 				
+				result[ch][kan] = datas[ch][kan];
+				sum  += datas[ch][kan];
+//				System.out.print(result.length-1);
+//				System.out.print(",");
+//				System.out.print(ch);
+//				System.out.print("=");
+//				System.out.print(datas[ch][kan]);
+				result[result.length-1][kan] += datas[ch][kan];
 			}
-			result[ch][datas.length+1] = widthSum;
-//			result[ch][datas.length] = heightSum;
-			System.out.println(Arrays.toString(result[ch]));
-//			System.out.println(result.length);
-		
+//			System.out.print(result[result.length-1][ch]);
+			result[ch][datas.length+1] = sum;
+			result[result.length-1][result[0].length-1] += sum;
 		}
 
-		
+		for(int ch=0;ch<result.length;ch++) {
+			System.out.println(Arrays.toString(result[ch]));
+		}
+
 	}
 
 }
