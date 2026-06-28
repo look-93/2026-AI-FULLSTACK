1e59a3194a3a3ed88997a03b24d62cc46e5b43e6
look-93 <qhfk0961@gmail.com>
2026-06-29 01:12:41 +0900

    수정

--- track007_boot_api/moitdev/src/main/java/com/the703/SampleController.java
+++ track007_boot_api/moitdev/src/main/java/com/the703/SampleController.java
@@ -5,23 +5,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SampleController {
-	// tmptAdmin 복사해서 content안에 본인 hrml 붙여넣으면됩니다
+	// tmptAdmin 복사해서 content안에 본인 html 붙여넣으면됩니다
+	
 	// 탬플릿 샘플
 	@GetMapping("/tmptAdmin")
-	public String tmpt() {return "tmptAdmin"; } //prefix(/templates) + tmpt + suffix(.html)
+	public String tmptAdmin() {return "tmptAdmin"; } //prefix(/templates) + tmpt + suffix(.html)
+	
+	@GetMapping("/tmptMain")
+	public String tmptMain() {return "tmptMain"; } //prefix(/templates) + tmpt + suffix(.html)
+
+	@GetMapping("/tmptMeetupList")
+	public String tmptMeetupList() {return "tmptMeetupList"; } //prefix(/templates) + tmpt + suffix(.html)
+	
+	@GetMapping("/tmptMypage")
+	public String tmptMypage() {return "tmptMypage"; } //prefix(/templates) + tmpt + suffix(.html)
 
 	// admin 페이지 샘플 
 	@GetMapping("/samplePage/admin")
 	public String admin() {return "/samplePage/admin"; }
 
-	/*
-	 * // mypage 페이지 샘플
-	 * 
-	 * @GetMapping("/fragments/myPageHeader") public String myPageHeader() {return
-	 * "/fragments/myPageHeader"; }
-	 */	
-
 	// main 페이지 샘플 
-	@GetMapping("/samplePage/meetupMain")
-	public String myPageHeader() {return "/samplePage/meetupMain"; }		
+	@GetMapping("/samplePage/main")
+	public String main() {return "/samplePage/main"; }
+
+	// meetupList 페이지 샘플 
+	@GetMapping("/samplePage/meetupList")
+	public String myPageHeader() {return "/samplePage/meetupList"; }		
+	
+	// mypage 페이지 샘플 
+	@GetMapping("/samplePage/mypage")
+	public String mypage() {return "/samplePage/mypage"; }			
+	
 }
